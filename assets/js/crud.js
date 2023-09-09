9391f8a3ebab546ba27171dfde7877a645f987cc
jarher <jefferh33@gmail.com>
2023-09-09 14:42:33 -0500

    modificando archivo crud.js!

--- assets/js/crud.js
+++ assets/js/crud.js
@@ -1,13 +1,11 @@
 async function getData() {
-  const res = (await fetch("https://github.com/jarher/interactive_comments/blob/main/data.json",{
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    mode: 'no-cors', // no-cors, *cors, same-origin
-    //cache: 'no-cache',  *default, no-cache, reload, force-cache, only-if-cached
-    //credentials: 'same-origin',  include, *same-origin, omit
-    //headers: {
-    //  'Content-Type': 'application/json',
-    //  'Access-Control-Allow-Origin': 'https://jarher.github.io/interactive_comments/'
-    //},
+  const res = (await fetch("../../data.json",{
+    method: "GET",
+    mode:'no-cors',
+    credentials:"same-origin",
+    headers:{
+      "Content-Type":"application/json"
+    }
   })).json();
   return res;
 }
