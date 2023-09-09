b88283405c2a47e9c178458de59ab771d153a2a5
Jeffer Andrés Rojas Herrera <jefferh33@gmail.com>
2023-09-09 13:07:18 -0500

    Update crud.js

--- assets/js/crud.js
+++ assets/js/crud.js
@@ -2,12 +2,12 @@ async function getData() {
   const res = (await fetch("https://github.com/jarher/interactive_comments/blob/main/data.json",{
     method: 'GET', // *GET, POST, PUT, DELETE, etc.
     mode: 'no-cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': 'https://jarher.github.io/interactive_comments/'
-    },
+    //cache: 'no-cache',  *default, no-cache, reload, force-cache, only-if-cached
+    //credentials: 'same-origin',  include, *same-origin, omit
+    //headers: {
+    //  'Content-Type': 'application/json',
+    //  'Access-Control-Allow-Origin': 'https://jarher.github.io/interactive_comments/'
+    //},
   })).json();
   return res;
 }
